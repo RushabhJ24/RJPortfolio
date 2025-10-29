fa52c3ad737d0f5061624bfdbcd85203f82c0b52
Rushabh <rushabhsjain40@gmail.com>
2025-10-29 15:33:23 +0530

    updated server.js

--- backend/server.js
+++ backend/server.js
@@ -31,7 +31,7 @@ app.use(cors({
   allowedHeaders: ["Content-Type"],
   credentials: false
 }));
-app.options('*', cors());
+app.options(/.*/, cors());
 
 app.use(express.json({ limit: '10kb' }));
 
